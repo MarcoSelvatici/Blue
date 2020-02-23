65a60396a9e38be9a4dd046a015fee523d2258bc
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2020-02-23 21:14:05 +0100

    add: limited functionApplication

--- sk3017/BetaEngine.fs
+++ sk3017/BetaEngine.fs
@@ -2,25 +2,96 @@ module BetaEngine
 
 open TokeniserParserStub
 
-type Enviourment = Map<string, Ast>
+type Enviourment = string list * Map<string, Ast>
 
-let rec reduceAst env ast =
+let extendEnv (boundVariables, variableMap) name body =
+    name::boundVariables, Map.add name body variableMap
+
+let extendVarMap (boundVariables, variableMap) name body = 
+    boundVariables, Map.add name body variableMap
+
+let extendBoundVar (boundVariables, variableMap) name = 
+    name::boundVariables, variableMap
+
+
+// TODO: delete - use result.map
+let passError f res = 
+    match res with
+    | Ok x -> Ok (f x)
+    | Error x -> res
+
+(*
+BuiltInFunc
+    / UnaryOp
+    | Not
+    | Head
+    | Tail
+    | Size
+    // ComparisonOp
+    | Greater
+    | GreaterEq
+    | Less
+    | LessEq
+    | Equal
+    // LogicalOp
+    | And
+    | Or
+    // AdditiveOp
+    | Plus
+    | Minus
+    // MultiplicativeOp
+    | Mult
+    | Div
+*)
+
+let rec functionApplication env f x =
+    match evaluate env f, evaluate env x with
+    | Error e, _ | _, Error e -> Error e
+    | Ok (Identifier i), Ok( ast ) -> Ok ( FuncApp (Identifier i, ast) )    // if function is an indetifier - don't evaluate
+    | Ok( ast ), Ok (Identifier i) -> Ok ( FuncApp (ast, Identifier i) )    // if argument is an indetifier - don't evaluate
+    | Ok (Lambda  { LambdaParam = name; LambdaBody = body;}), Ok( ast ) ->  // reduce lambda terms
+        evaluate (extendVarMap env name ast) body
+    // add FuncDefExp
+    // add FuncApp
+    // add BuiltInFunc
+    // add Null / Literal / IdentifierList
+    | _ -> Error "Not impemented" 
+     
+
+(*
+type Ast =
+    | FuncDefExp of FuncDefExpType // function definition(s) followed by expression
+x   | Lambda of LambdaType // anonymous function
+    | FuncApp of Ast * Ast
+    | FuncAppList of Ast list
+    | Null // used with pair to make lists
+    | Literal of Literal
+x    | Identifier of string
+    | IdentifierList of string list
+    | BuiltInFunc of BuiltInFunc // E.g. builtinTimes, builtinPlus
+    | RoundExp of Ast // possibly needed see techical note
+    | IfExp of Ast * Ast * Ast
+    | SeqExp of Ast * Ast // A pair of two elements [a, b]. TODO: (syntactic sugar) Extend this to (untyped) lists [a, b, c, d] -> Seq(a, Seq(b, ...))
+*)
+
+and evaluate env ast =
     match ast with
-    | FuncDefExp {FuncName = name; FuncBody = body; Rest = rest}    -> 
-        reduceAst (Map.add name body env) rest
-    | Lambda l          -> failwithf "Lambda - Not implemented %A" ast  // if no outermost can be called we call inside // closures?
-    | FuncApp (f,x)     -> failwithf "FuncApp - Not implemented %A" ast 
-    | RoundExp x        -> reduceAst env x
+    | FuncDefExp {FuncName = name; FuncBody = body; Rest = rest} -> 
+        evaluate (extendEnv env name body) rest
+    | Lambda  { LambdaParam = name; LambdaBody = body;}-> 
+        evaluate (extendBoundVar env name) body
+    | FuncApp (f,x)     -> functionApplication env f x 
+    | RoundExp x        -> evaluate env x
     | IfExp (bool,bTrue,bFalse) ->
-        match reduceAst env bool with
-        | Ok (Literal (BoolLit true))  -> reduceAst env bTrue
-        | Ok (Literal (BoolLit false)) -> reduceAst env bFalse
-        | error -> error
+        match evaluate env bool with
+        | Ok (Literal (BoolLit true))  -> evaluate env bTrue
+        | Ok (Literal (BoolLit false)) -> evaluate env bFalse
+        | error -> error    
     | Identifier i -> 
-        Map.tryFind i env |>
-        function
-        | Some def -> reduceAst env def
-        | None -> Error <| sprintf "Identifier \'%s\' is not defined" i
+        match Map.tryFind i (snd env) with
+        | Some ast -> evaluate env ast
+        | None when List.contains i (fst env) -> Ok (Identifier i)
+        | None -> Error <| sprintf "Identifier \'%s\' is not defined" i;
     | Null | Literal _ | BuiltInFunc _ | SeqExp _ 
                        -> Ok ast
     | FuncAppList _     -> Error "What? parser returned FuncAppList"  
@@ -29,4 +100,4 @@ let rec reduceAst env ast =
 
 // top level function
 let runAst ast =
-    reduceAst Map.empty ast
+    evaluate ([],Map.empty) ast
