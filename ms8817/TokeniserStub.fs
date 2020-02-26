645e9d5bd50b85ad67b8612be20a913512ef1e79
ms8817 <ms8817@ic.ac.uk>
2020-02-26 22:40:34 +0000

    Add comments in tokeniser stub.

--- ms8817/TokeniserStub.fs
+++ ms8817/TokeniserStub.fs
@@ -1,11 +1,10 @@
-// Note: this is in way supposed to be an official version.
-// I just need something to get my types to work in the parser.
-// Many changes are probably required.
+// Author: Marco Selvatici (ms8817)
+
+// Note: this is not supposed to be an official version of the tokeniser types.
+// I just need some types to work on the parser.
+// Fabio will take over and finalise the type definition.
 
 module TokeniserStub
-
-let print x =
-    printfn "%A" x
 
 type BuiltInFunc =
     // Builtin with no special treatment
@@ -13,8 +12,6 @@ type BuiltInFunc =
     | Head // 'a list -> 'a
     | Tail // 'a list -> 'a list
     | Size // 'a list -> int
-    | Implode // Reduce a list of chars into a string. string list -> string
-    | Explode // Expand a string into a list of chars. string -> string list
     | Append // Append to list. 'a -> 'a List -> 'a List
     | StrEq // Comparison between two strings. string -> string -> bool
     // ComparisonOp
