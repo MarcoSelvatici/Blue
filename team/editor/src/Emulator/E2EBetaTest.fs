1fc295d10ae68b53473de44a1906d9eb704450fb
= <=>
2020-03-07 19:02:08 +0000

    some list function testings

--- team/editor/src/Emulator/E2EBetaTest.fs
+++ team/editor/src/Emulator/E2EBetaTest.fs
@@ -18,18 +18,90 @@ let testCasesBetaE2E : (string * string * Result<Ast, SharedTypes.ErrorT>) list 
     "Curried Lambda", "\xy.42",
     Ok (LambdaExp { LambdaParam = "xy"
                     LambdaBody = Literal (IntLit 42) });
-    "ListMap library function", "let listMap f lst = 
-                                    if size lst == 0
-                                    then []
-                                    else append (f (head lst))
-                                         (listMap f (tail lst))
-                                    fi
-                                  in 
-                                  listMap (\x.x+1)[1, 2, 3]
-                                  ni",
+    "ListMap add ", "let listMap f lst = 
+                      if size lst == 0
+                      then []
+                      else append (f (head lst))
+                           (listMap f (tail lst))
+                      fi
+                    in 
+                    listMap (\x.x+1)[1, 2, 3]
+                    ni",
     Ok (SeqExp
             (Literal (IntLit 2),
-            SeqExp (Literal (IntLit 3),SeqExp (Literal (IntLit 4),SeqExp (Null,Null)))))
+            SeqExp (Literal (IntLit 3),SeqExp (Literal (IntLit 4),SeqExp (Null,Null)))));
+    "ListMap mult ", "let listMap f lst = 
+                        if size lst == 0
+                        then []
+                        else append (f (head lst))
+                             (listMap f (tail lst))
+                        fi
+                      in 
+                      listMap (\x.x*2)[3, 5, 32]
+                      ni",
+    Ok (SeqExp
+            (Literal (IntLit 6),
+            SeqExp (Literal (IntLit 10),SeqExp (Literal (IntLit 64),SeqExp (Null,Null)))));
+    "ListMap div ", "let listMap f lst = 
+                        if size lst == 0
+                        then []
+                        else append (f (head lst))
+                             (listMap f (tail lst))
+                        fi
+                      in 
+                      listMap (\x.x/2)[10, 2, 84]
+                      ni",
+    Ok (SeqExp
+            (Literal (IntLit 5),
+            SeqExp (Literal (IntLit 1),SeqExp (Literal (IntLit 42),SeqExp (Null,Null)))))
+    "ListMap sub ", "let listMap f lst = 
+                        if size lst == 0
+                        then []
+                        else append (f (head lst))
+                             (listMap f (tail lst))
+                        fi
+                      in 
+                      listMap (\x.x-2)[2, 1, 11]
+                      ni",
+    Ok (SeqExp
+            (Literal (IntLit 0),
+            SeqExp (Literal (IntLit -1),SeqExp (Literal (IntLit 9),SeqExp (Null,Null)))));
+    "ListFold add ", "let listFold f acc lst = 
+                        if size lst == 0
+                        then acc
+                        else listFold f (f acc (head lst)) (tail lst)
+                        fi
+                      in
+                          listFold(\x y.x+y) 0 [10, 2, 12]
+                      ni",
+    Ok  (Literal (IntLit 24));
+    "ListFold sub ", "let listFold f acc lst = 
+                        if size lst == 0
+                        then acc
+                        else listFold f (f acc (head lst)) (tail lst)
+                        fi
+                      in
+                          listFold(\x y.x-y) 36 [10, 2, 12]
+                      ni",
+    Ok  (Literal (IntLit 12));
+    "ListFold mul ", "let listFold f acc lst = 
+                        if size lst == 0
+                        then acc
+                        else listFold f (f acc (head lst)) (tail lst)
+                        fi
+                      in
+                          listFold(\x y.x*y) 1 [5, 2, 14]
+                      ni",
+    Ok  (Literal (IntLit 140));
+    "ListFold div ", "let listFold f acc lst = 
+                        if size lst == 0
+                        then acc
+                        else listFold f (f acc (head lst)) (tail lst)
+                        fi
+                      in
+                          listFold(\x y.x/y) 120 [4, 3, 2]
+                      ni",
+    Ok  (Literal (IntLit 5));
 
 ]
   
